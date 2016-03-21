5c3c456486adec79aeef8e6c79cbec469e565817
Yufi <yfhj1990@hotmail.com>
2016-03-21 23:12:43 +0800

    Update router.

--- Frontend/Application/View/Home/Nav.elm
+++ Frontend/Application/View/Home/Nav.elm
@@ -1,30 +1,35 @@
-module Application.View.Home.Nav (navView) where
+module Application.View.Home.Nav (view) where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import String exposing (split, toUpper)
+import List exposing (map)
 import Application.Style.Color exposing (fcPc, fcG3)
+import Application.Sitemap exposing (..)
 
-
-navView : Html
-navView =
-  nav [navigateStyle]
-        [ ul [ menuStyle ]
-            [ li [ menuItemStyle ]
-                [ a []
-                    [ text "Not"
-                    , span [ jColor ] [ text "E" ]
-                    , text " on"
-                    ]
-                ]
-            , li [ menuItemStyle ]
-                [ a []
-                    [ text "Fin"
-                    , span [ jColor ] [ text "D" ]
-                    , text " me"
-                    ]
-                ]
-            ]
+view : (List Html -> Html) -> (List Html -> Html) -> Html
+view link1 link2 =
+  nav
+    [navigateStyle]
+    [ ul
+        [ class "listReset" ]
+        [ listItemView <| link1 <| linkerView "Note on" "e"
+        , listItemView <| link2 <| linkerView "Find me" "d"
         ]
+    ]
+
+linkerView : String -> String -> List Html
+linkerView str char =
+  let
+    dom c =
+      if c == char
+      then span [ redColor ] [ text <| toUpper c ]
+      else span [] [ text c ]
+  in
+    map dom <| split "" str
+
+listItemView : Html -> Html
+listItemView linker = li [ listItemStyle ] [ linker ]
 
 navigateStyle : Attribute
 navigateStyle =
@@ -33,21 +38,13 @@ navigateStyle =
         , ("right", "50%")
         ]
 
-menuStyle : Attribute
-menuStyle =
-  style [ ("list-style-type", "none")
-        , ("padding", "0")
-        , ("margin", "0")
-        ]
-
-menuItemStyle : Attribute
-menuItemStyle =
-  style [ ("font-size", "1.4rem")
-        , fcG3
+listItemStyle : Attribute
+listItemStyle =
+  style [ ("margin", "0.5rem 0")
+        , ("font-size", "1.4rem")
         , ("font-weight", "100")
-        , ("margin", "0.5rem 0")
+        , fcG3
         ]
 
-jColor : Attribute
-jColor =
-  style [ fcPc ]
+redColor : Attribute
+redColor = style [ fcPc ]
