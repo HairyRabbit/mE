6ab56abf554b1d76ca98c25f3301b74c2d36bfe8
Yuffiy <yfhj1990@hotmail.com>
2016-04-02 17:58:26 +0800

    Add posts model.

---
+++ Frontend/Application/Newsletter/Model.elm
@@ -0,0 +1,2 @@
+module Newsletter.Model where
+
--- Frontend/Application/Posts/Model.elm
+++ Frontend/Application/Posts/Model.elm
@@ -1,2 +1,10 @@
 module Posts.Model where
 
+type Newsletter =
+  { id : String
+  , datetime : String
+  , title : String
+  }
+
+type alias Model = List Newsletter
+
