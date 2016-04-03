a494112a9b08bba367363812238235a2a9c9ce77
Yufi <yfhj1990@hotmail.com>
2016-04-03 22:42:21 +0800

    Add Posts update,init,action,model

--- Frontend/Application/Init.elm
+++ Frontend/Application/Init.elm
@@ -5,6 +5,7 @@ import Model exposing (Model)
 import Action exposing (Action(..))
 import Router.Init as Router
 import Home.Init as Home
+import Posts.Init as Posts
 
 
 init : String -> (Model, Effects Action)
@@ -12,12 +13,14 @@ init path =
   let
     (modelRouter, effectsRouter) = Router.init path
     (modelHome, effectsHome) = Home.init
+    (modelPosts, effectsPosts) = Posts.init
 
-    models = Model modelRouter modelHome
+    models = Model modelRouter modelHome modelPosts
     effects =
       Effects.batch
                [ Effects.map ActionRouter effectsRouter
                , Effects.map ActionHome effectsHome
+               , Effects.map ActionPosts effectsPosts
                ]
   in
     (models, effects)
--- Frontend/Application/Model.elm
+++ Frontend/Application/Model.elm
@@ -2,9 +2,11 @@ module Model where
 
 import Router.Model as Router
 import Home.Model   as Home
+import Posts.Model  as Posts
 
 
 type alias Model =
   { router : Router.Model
   , home   : Home.Model
+  , posts  : Posts.Model
   }
