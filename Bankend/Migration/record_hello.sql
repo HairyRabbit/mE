f6a274aecaf6413e6353a150f4d6e4b226792398
Yuffiy <yfhj1990@hotmail.com>
2016-04-21 16:57:26 +0800

    Testing.

---
+++ Bankend/Migration/record_hello.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+INSERT INTO post(
+  title,
+  istop,
+  intro,
+  filepath
+)
+VALUES(
+  'Hi.这里是兔子',
+  true,
+  'Hi，这里是兔子，嗯就是这样，我的 Blog 和大家见面了。跳票了那么久还是要先说一声，so sorry。哦对了，我是一个Frontend Developer，喜欢reding，喜欢coding，喜欢折腾各类黑科技，据说这次的改版有不少新东西呢，快点进来看看吧！',
+  'hello'
+)
+
+
+END;
+$$
+LANGUAGE plpgsql;
